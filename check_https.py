00872c1e685b74ebefe45a664abbeb9df71e6fb0
kyhwana <kyhwana@gmail.com>
2015-05-31 13:04:06 +1200

    Change user agent/remove analytics.

--- check_https.py
+++ check_https.py
@@ -19,17 +19,8 @@ import requests
 from lxml import etree, html
 
 PARALLELISM = 16
-USER_AGENT = "Mozilla/5.0 compatible HTTPSWatch Bot https://httpswatch.com"
-ANALYTICS = """<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-342043-4', 'auto');
-ga('send', 'pageview');
-</script>
-"""
+USER_AGENT = "Mozilla/5.0 compatible HTTPSWatch Bot https://httpswatch.co.nz"
+ANALYTICS = ""
 META_XPATH = etree.XPath(
     "//meta[not(ancestor::noscript) and re:test(@http-equiv, \"^refresh$\", \"i\")]",
     namespaces={"re": "http://exslt.org/regular-expressions"}
